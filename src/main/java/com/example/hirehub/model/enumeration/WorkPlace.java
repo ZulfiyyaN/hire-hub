776c3cc658e5468bdb17ed1d978fb5237e64b584
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-14 00:28:24 +0400

    Create JobPosting entities, WorkPlace and WorkType enums

---
+++ src/main/java/com/example/hirehub/model/enumeration/WorkPlace.java
@@ -0,0 +1,7 @@
+package com.example.hirehub.model.enumeration;
+
+public enum WorkPlace {
+    REMOTE,
+    ON_SITE,
+    HYBRID;
+}
---
+++ src/main/java/com/example/hirehub/model/enumeration/WorkType.java
@@ -0,0 +1,10 @@
+package com.example.hirehub.model.enumeration;
+
+public enum WorkType {
+    FULLTIME,
+    PARTTIME,
+    CONTRACT,
+    FREELANCE,
+    VOLUNTEER;
+
+}
