2f4c3ca301f2ea5f5182a4df68e113b10d213506
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-15 15:46:35 +0400

    add import and modified filterChain in SecurityConfig

--- src/main/java/com/example/hirehub/config/SecurityConfig.java
+++ src/main/java/com/example/hirehub/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.hirehub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,10 +22,10 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)  {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                      //  .requestMatchers()
+                        .requestMatchers("/api/candidate/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
