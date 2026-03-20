3311bd1833763517a6cc694ce2aa0599dc460cd5
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-20 20:42:01 +0400

    created CandidateDetailsService and CandidateSecurity and overrided some methods

---
+++ src/main/java/com/example/hirehub/security/CandidateDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.hirehub.security;
+
+import com.example.hirehub.model.entity.CandidateEntity;
+import com.example.hirehub.repository.CandidateRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class CandidateDetailsService implements UserDetailsService {
+
+    CandidateRepository candidateRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        Optional<CandidateEntity> optionalcandidate = candidateRepository.findByEmail(email);
+        if (optionalcandidate.isPresent()) {
+            log.info("Candidate is found with {}", email);
+            return new CandidateSecurity(optionalcandidate.get());
+        } else {
+            log.warn("Username is not found with {}", email);
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+}
