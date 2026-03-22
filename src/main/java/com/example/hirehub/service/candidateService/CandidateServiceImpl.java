c994175ff8a009e4c6c66705412ed0424105a9de
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-22 14:31:12 +0400

    modified imports in CandidateServiceImpl.java

--- src/main/java/com/example/hirehub/service/candidateService/CandidateServiceImpl.java
+++ src/main/java/com/example/hirehub/service/candidateService/CandidateServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.hirehub.service.candidateService;
 
 import com.example.hirehub.exception.AlreadyExistsException;
-import com.example.hirehub.mapper.CandidateMapper;
+import com.example.hirehub.exception.CandidateNotFoundException;
+import com.example.hirehub.mapper.CandidateMapperForRegister;
+import com.example.hirehub.mapper.CandidateMapperForUpdate;
 import com.example.hirehub.model.entity.CandidateEntity;
+import com.example.hirehub.model.entity.CandidateInfoEntity;
 import com.example.hirehub.model.request.CandidateRegisterRequest;
+import com.example.hirehub.model.request.CandidateUpdateRequest;
 import com.example.hirehub.model.response.CandidateRegisterResponse;
+import com.example.hirehub.model.response.CandidateUpdateResponse;
 import com.example.hirehub.repository.CandidateRepository;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
@@ -14,6 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @Slf4j
 @Transactional
@@ -22,7 +30,8 @@ import org.springframework.stereotype.Service;
 
 public class CandidateServiceImpl implements CandidateService {
     CandidateRepository candidateRepository;
-    CandidateMapper candidateMapper;
+    CandidateMapperForRegister candidateMapper;q
+    CandidateMapperForUpdate candidateMapperForUpdate;
     PasswordEncoder passwordEncoder;
 
     @Override
@@ -35,12 +44,55 @@ public class CandidateServiceImpl implements CandidateService {
             log.warn("Phone {} already exists", request.getPhone());
             throw new AlreadyExistsException("Candidate with this phone already exists");
         }
-
         CandidateEntity candidateEntity = candidateMapper.toCandidate(request);
-       candidateEntity =  candidateRepository.save(candidateEntity);
+        candidateEntity = candidateRepository.save(candidateEntity);
         CandidateRegisterResponse response = candidateMapper.toResponse(candidateEntity);
-        log.info("{} is registered", request.getName() + " "+ request.getSurname());
+        log.info("{} is registered", request.getName() + " " + request.getSurname());
         return response;
+    }
 
+
+    @Override
+    public CandidateUpdateResponse candidateUpdate(String email, CandidateUpdateRequest request) {
+
+        Optional<CandidateEntity> entity = candidateRepository.findByEmail(email);
+        if (entity.isEmpty()) {
+            log.warn("Candidate not found with {} ", email);
+            throw new CandidateNotFoundException("Candidate not found: " + email);
+        }
+        if (request.getName() != null) {
+            entity.get().setName(request.getName());
+        }
+        if (request.getSurname() != null) {
+            entity.get().setSurname(request.getSurname());
+        }
+        if (request.getPhone() != null) {
+            entity.get().setPhone(request.getPhone());
+        }
+        if (request.getGender() != null) {
+            entity.get().setGender(request.getGender());
+        }
+        entity.get().setLastUpdate(LocalDateTime.now());
+
+        CandidateInfoEntity info = entity.get().getCandidateInfo();
+
+        if (request.getDateOfBirth() != null) {
+            info.setDateOfBirth(request.getDateOfBirth());
+        }
+        if (request.getEducation() != null) {
+            info.setEducation(request.getEducation());
+        }
+        if (request.getWorkExperience() != null) {
+            info.setWorkExperience(request.getWorkExperience());
+        }
+        if (request.getKnowledge() != null) {
+            info.setKnowledge(request.getKnowledge());
+        }
+
+        entity.get().setCandidateInfo(info);
+        info.setCandidate(entity.get());
+
+        CandidateUpdateResponse response = candidateMapperForUpdate.toResponseForUpdate(entity.get());
+        return response;
     }
 }
