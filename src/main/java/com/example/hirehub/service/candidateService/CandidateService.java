c602fa0095b5aabe256d52c9b7dcc2df1acddbdf
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-22 14:28:35 +0400

    created candidateUpdate method and added some related imports to CandidateService.java

--- src/main/java/com/example/hirehub/service/candidateService/CandidateService.java
+++ src/main/java/com/example/hirehub/service/candidateService/CandidateService.java
@@ -1,11 +1,15 @@
 package com.example.hirehub.service.candidateService;
 
 import com.example.hirehub.model.request.CandidateRegisterRequest;
+import com.example.hirehub.model.request.CandidateUpdateRequest;
 import com.example.hirehub.model.response.CandidateRegisterResponse;
+import com.example.hirehub.model.response.CandidateUpdateResponse;
 
 public interface CandidateService {
 
 
     public CandidateRegisterResponse candidateRegister(CandidateRegisterRequest request);
+
+    public CandidateUpdateResponse candidateUpdate(String email, CandidateUpdateRequest request);
 
 }
