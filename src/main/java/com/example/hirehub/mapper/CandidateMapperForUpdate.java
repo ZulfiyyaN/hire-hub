e90e54cc3ddc4f4b24bfc50107dbecafec65bed2
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-23 21:25:37 +0400

    added some imports to CandidateMapperForUpdate.java

--- src/main/java/com/example/hirehub/mapper/CandidateMapperForUpdate.java
+++ src/main/java/com/example/hirehub/mapper/CandidateMapperForUpdate.java
@@ -1,9 +1,7 @@
 package com.example.hirehub.mapper;
 
-import com.example.hirehub.exception.CandidateNotFoundException;
-import com.example.hirehub.model.entity.CandidateEntity;
-import com.example.hirehub.model.entity.CandidateInfoEntity;
-import com.example.hirehub.model.request.CandidateUpdateRequest;
+import com.example.hirehub.model.entity.candidateEntities.CandidateEntity;
+import com.example.hirehub.model.entity.candidateEntities.CandidateInfoEntity;
 import com.example.hirehub.model.response.CandidateUpdateResponse;
 import com.example.hirehub.repository.CandidateRepository;
 import lombok.AccessLevel;
@@ -11,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
