4977c8b10027e9438b4f606e5a24ac1e8e211142
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-23 21:21:17 +0400

    moved CompanyEntity.java to companyentites package and added some imports

--- src/main/java/com/example/hirehub/model/entity/companyEntities/CompanyEntity.java
+++ src/main/java/com/example/hirehub/model/entity/companyEntities/CompanyEntity.java
@@ -1,5 +1,6 @@
-package com.example.hirehub.model.entity;
+package com.example.hirehub.model.entity.companyEntities;
 
+import com.example.hirehub.model.entity.candidateEntities.CandidatePasswordEntity;
 import com.example.hirehub.model.enumeration.Status;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -18,24 +19,19 @@ public class CompanyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-
     String name;
-
     @Column(nullable = false, unique = true)
     String email;
-
-    String location;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     Status status = Status.PENDING; //by default
-
     @Column(name = "created_at", updatable = false)
     LocalDateTime createdAt = LocalDateTime.now();
-
-
     @OneToOne(mappedBy = "company", cascade = CascadeType.ALL)
     private CompanyInfoEntity companyInfo;
+
+    @OneToOne(mappedBy = "company", cascade = CascadeType.ALL)
+    CompanyPasswordEntity companyPasswordEntity;
 
 
 }
