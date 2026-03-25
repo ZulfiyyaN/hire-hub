bebd5f029a1c723f383fea88a33b45bfd3053fe6
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-25 14:26:22 +0400

    add company update method and rename register method to CompanyService.java

--- src/main/java/com/example/hirehub/service/companyService/CompanyService.java
+++ src/main/java/com/example/hirehub/service/companyService/CompanyService.java
@@ -1,10 +1,15 @@
 package com.example.hirehub.service.companyService;
 
+import com.example.hirehub.model.request.candidateRequest.CandidateUpdateRequest;
 import com.example.hirehub.model.request.companyRequest.CompanyRegisterRequest;
+import com.example.hirehub.model.request.companyRequest.CompanyUpdateRequest;
+import com.example.hirehub.model.response.CandidateUpdateResponse;
 import com.example.hirehub.model.response.CompanyRegisterResponse;
+import com.example.hirehub.model.response.CompanyUpdateResponse;
 
 public interface CompanyService {
 
-    public CompanyRegisterResponse register (CompanyRegisterRequest request);
+    public CompanyRegisterResponse companyRegister (CompanyRegisterRequest request);
+    public CompanyUpdateResponse companyUpdate(String email, CompanyUpdateRequest request);
 
 }
