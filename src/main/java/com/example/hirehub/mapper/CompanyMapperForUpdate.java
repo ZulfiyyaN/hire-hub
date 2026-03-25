c291ee86f8c23ad78abb41c61f0c0159d9a16155
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-25 14:53:17 +0400

    created company mapper for updating

---
+++ src/main/java/com/example/hirehub/mapper/CompanyMapperForUpdate.java
@@ -0,0 +1,38 @@
+package com.example.hirehub.mapper;
+
+import com.example.hirehub.model.entity.candidateEntities.CandidateEntity;
+import com.example.hirehub.model.entity.candidateEntities.CandidateInfoEntity;
+import com.example.hirehub.model.entity.companyEntities.CompanyEntity;
+import com.example.hirehub.model.entity.companyEntities.CompanyInfoEntity;
+import com.example.hirehub.model.response.CandidateUpdateResponse;
+import com.example.hirehub.model.response.CompanyUpdateResponse;
+import com.example.hirehub.repository.CandidateRepository;
+import com.example.hirehub.repository.CompanyRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE, makeFinal = true)
+public class CompanyMapperForUpdate {
+    CompanyRepository companyRepository;
+
+    public CompanyUpdateResponse toResponseForUpdate(CompanyEntity entity) {
+        CompanyInfoEntity info = entity.getCompanyInfo();
+        CompanyUpdateResponse response = new CompanyUpdateResponse();
+
+        response.setName(entity.getName());
+        response.setLocation(entity.getCompanyInfo().getLocation());
+        response.setWebsite(entity.getCompanyInfo().getWebsite());
+        response.setDescription(entity.getCompanyInfo().getDescription());
+        response.setFoundedAt(entity.getCompanyInfo().getFoundedAt());
+
+        return response;
+    }
+
+
+}
