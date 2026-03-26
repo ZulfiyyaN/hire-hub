b574413f6aaf2d44fcbb183241cfe24832367300
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-26 22:28:07 +0400

    Added some imports and some fields

---
+++ src/main/java/com/example/hirehub/model/response/jobPostingResponse/JobPostingCreateResponse.java
@@ -0,0 +1,25 @@
+package com.example.hirehub.model.response.jobPostingResponse;
+
+import com.example.hirehub.model.enumeration.WorkPlace;
+import com.example.hirehub.model.enumeration.WorkType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class JobPostingCreateResponse {
+
+    String companyName;
+    String companyEmail;
+    String jobTitle;
+    String eduReq;      //Education Requirements
+    String expLevel;//Experience Level
+    String skills;     //Skills & Qualifications
+    String location;
+    WorkType workType;
+    WorkPlace workPlace;  //Remote, On-Site or Hybrid
+    Double salary;
+    String position;
+    LocalDateTime expiredDate;
+}
