4bf6d34f76d6249590b0ecec56c1fabef0977090
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-26 22:26:45 +0400

    clean up unused imports and add Lombok annotations to JobPostingCreateRequest

--- src/main/java/com/example/hirehub/model/request/jobPostingRequest/JobPostingCreateRequest.java
+++ src/main/java/com/example/hirehub/model/request/jobPostingRequest/JobPostingCreateRequest.java
@@ -1,17 +1,15 @@
 package com.example.hirehub.model.request.jobPostingRequest;
 
-import com.example.hirehub.model.entity.companyEntities.CompanyEntity;
-import com.example.hirehub.model.entity.jobPostingEntities.JobPostingInfoEntity;
-import com.example.hirehub.model.enumeration.Status;
 import com.example.hirehub.model.enumeration.WorkPlace;
 import com.example.hirehub.model.enumeration.WorkType;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
-import org.springframework.data.annotation.LastModifiedDate;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class JobPostingCreateRequest {
 
     @NotBlank(message = "Job title can not be blank!")
