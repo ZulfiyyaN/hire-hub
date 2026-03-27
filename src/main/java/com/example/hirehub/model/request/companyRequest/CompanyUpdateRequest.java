5e2664833bf70879b40f36c6aa5b93ce1ab1c9cb
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-27 22:18:14 +0400

    delete validation in company update request

--- src/main/java/com/example/hirehub/model/request/companyRequest/CompanyUpdateRequest.java
+++ src/main/java/com/example/hirehub/model/request/companyRequest/CompanyUpdateRequest.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 @Setter
 public class CompanyUpdateRequest {
 
-    @NotBlank(message = "Name can not be blank!")
     String name;
     String location;
     String website;
