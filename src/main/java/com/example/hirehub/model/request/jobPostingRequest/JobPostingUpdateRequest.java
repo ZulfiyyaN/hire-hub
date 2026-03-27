664f512d19964ed0ed6fe5ca4e4fb1c88e8f25d0
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-27 22:12:50 +0400

    creat request for update job posting

---
+++ src/main/java/com/example/hirehub/model/request/jobPostingRequest/JobPostingUpdateRequest.java
@@ -0,0 +1,27 @@
+package com.example.hirehub.model.request.jobPostingRequest;
+
+import com.example.hirehub.model.enumeration.WorkPlace;
+import com.example.hirehub.model.enumeration.WorkType;
+import jakarta.validation.constraints.Future;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class JobPostingUpdateRequest {
+
+    String jobTitle;
+    String eduReq;      //Education Requirements
+    String expLevel;//Experience Level
+    String skills;     //Skills & Qualifications
+    String location;
+    WorkType workType;
+    WorkPlace workPlace;  //Remote, On-Site or Hybrid
+    Double salary;
+    String position;
+    @Future(message = "Expired date should be future date")
+    LocalDateTime expiredDate;
+
+}
