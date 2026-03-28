4bdb2f709d8a9bf4d605454bd15ccedd5240c39b
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-28 13:43:59 +0400

    add FieldDefaults to JobPostingService and update imports for job posting entities

--- src/main/java/com/example/hirehub/service/jobPostingService/JobPostingServiceImpl.java
+++ src/main/java/com/example/hirehub/service/jobPostingService/JobPostingServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.hirehub.service.jobPostingService;
 import com.example.hirehub.exception.CompanyNotFoundException;
 import com.example.hirehub.mapper.JobPostingMapperForCreate;
 import com.example.hirehub.mapper.JobPostingMapperForUpdate;
+import com.example.hirehub.model.entity.companyEntities.CompanyEntity;
 import com.example.hirehub.model.entity.jobPostingEntities.JobPostingEntity;
+import com.example.hirehub.model.entity.jobPostingEntities.JobPostingInfoEntity;
 import com.example.hirehub.model.request.jobPostingRequest.JobPostingCreateRequest;
 import com.example.hirehub.model.request.jobPostingRequest.JobPostingUpdateRequest;
 import com.example.hirehub.model.response.jobPostingResponse.JobPostingCreateResponse;
@@ -11,14 +13,19 @@ import com.example.hirehub.model.response.jobPostingResponse.JobPostingUpdateRes
 import com.example.hirehub.repository.CompanyRepository;
 import com.example.hirehub.repository.JobPostingRepository;
 import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
 @Transactional
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class JobPostingServiceImpl implements JobPostingService {
     CompanyRepository companyRepository;
     JobPostingMapperForCreate jobPostingMapperForCreate;
@@ -27,30 +34,64 @@ public class JobPostingServiceImpl implements JobPostingService {
 
     @Override
     public JobPostingCreateResponse createJobPost(String email, JobPostingCreateRequest request) {
-        if (!companyRepository.existsByEmail(email)) {
+        Optional<CompanyEntity> companyEntity = companyRepository.findByEmail(email);
+        if (companyEntity.isEmpty()) {
             log.warn("Email is wrong!");
             throw new CompanyNotFoundException("Company not found!");
         }
         JobPostingEntity entity = jobPostingMapperForCreate.toEntity(request);
+        entity.setCompany(companyEntity.get());
+        jobPostingRepository.save(entity);
         JobPostingCreateResponse response = jobPostingMapperForCreate.toResponse(entity);
 
-        jobPostingRepository.save(entity);
         return response;
     }
 
     @Override
     public JobPostingUpdateResponse updateJobPost(String email, JobPostingUpdateRequest request) {
-        if (!companyRepository.existsByEmail(email)) {
+        Optional<CompanyEntity> companyEntity = companyRepository.findByEmail(email);
+        if (companyEntity.isEmpty()) {
             log.warn("Email is wrong!");
             throw new CompanyNotFoundException("Company not found!");
         }
+        JobPostingEntity entity = new JobPostingEntity();
+        if (request.getJobTitle() != null) {
+            entity.setJobTitle(request.getJobTitle());
+        }
+        if (request.getExpiredDate() != null) {
+            entity.setExpiredDate(request.getExpiredDate());
+        }
+        JobPostingInfoEntity infoEntity = new JobPostingInfoEntity();
+        if (request.getEduReq() != null) {
+            infoEntity.setEduReq(request.getEduReq());
+        }
+        if (request.getExpLevel() != null) {
+            infoEntity.setExpLevel(request.getExpLevel());
+        }
+        if (request.getSkills() != null) {
+            infoEntity.setSkills(request.getSkills());
+        }
+        if (request.getLocation() != null) {
+            infoEntity.setLocation(request.getLocation());
+        }
+        if (request.getWorkType() != null) {
+            infoEntity.setWorkType(request.getWorkType());
+        }
+        if (request.getWorkPlace() != null) {
+            infoEntity.setWorkPlace(request.getWorkPlace());
+        }
+        if (request.getSalary() != null) {
+            infoEntity.setSalary(request.getSalary());
+        }
+        if (request.getPosition() != null) {
+            infoEntity.setPosition(request.getPosition());
+        }
 
-        JobPostingEntity entity = jobPostingMapperForUpdate.toEntityForUpdate(request);
-        JobPostingUpdateResponse response = jobPostingMapperForUpdate.toResponseForUpdate(entity);
+        entity.setJobPostingInfoEntity(infoEntity);
+        entity.setCompany(companyEntity.get());
+        infoEntity.setJobPostingEntity(entity);
         jobPostingRepository.save(entity);
-
+        JobPostingUpdateResponse response = jobPostingMapperForUpdate.toResponseForUpdate(entity);
         return response;
-
-
     }
 }
