ff14aafd4be8169ce522ad25a8cf8504aa0c367b
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-28 13:53:54 +0400

    update create method and create update method in JobPostingController.java

--- src/main/java/com/example/hirehub/controller/JobPostingController.java
+++ src/main/java/com/example/hirehub/controller/JobPostingController.java
@@ -1,17 +1,18 @@
 package com.example.hirehub.controller;
 
+import com.example.hirehub.model.request.companyRequest.CompanyUpdateRequest;
 import com.example.hirehub.model.request.jobPostingRequest.JobPostingCreateRequest;
+import com.example.hirehub.model.request.jobPostingRequest.JobPostingUpdateRequest;
 import com.example.hirehub.model.response.jobPostingResponse.JobPostingCreateResponse;
+import com.example.hirehub.model.response.jobPostingResponse.JobPostingUpdateResponse;
 import com.example.hirehub.service.jobPostingService.JobPostingService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/job_posting")
@@ -21,9 +22,22 @@ public class JobPostingController {
     JobPostingService jobPostingService;
 
     @PostMapping("/create")
-    public ResponseEntity<JobPostingCreateResponse> createJobPost(String email, @PathVariable @Valid JobPostingCreateRequest request){
+    public ResponseEntity<JobPostingCreateResponse> createJobPost(@RequestBody @Valid JobPostingCreateRequest request,
+                                                                  Authentication authentication){
+        String email = authentication.getName();
        JobPostingCreateResponse response =  jobPostingService.createJobPost(email, request);
+        System.out.println(email);
         return ResponseEntity.ok(response);
     }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<JobPostingUpdateResponse> updateJobPost(@RequestBody @Valid JobPostingUpdateRequest request,
+                                                                  Authentication authentication ){
+        String email = authentication.getName();
+        JobPostingUpdateResponse response = jobPostingService.updateJobPost(email, request);
+        return  ResponseEntity.ok(response);
+    }
+
 
 }
