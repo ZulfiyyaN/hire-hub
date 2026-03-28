5a7fbbd23df1ee8d472281c616bb4f9b0e1ad4cf
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-28 13:41:50 +0400

    add annotations in JobPostingMapperForUpdate.java

--- src/main/java/com/example/hirehub/mapper/JobPostingMapperForUpdate.java
+++ src/main/java/com/example/hirehub/mapper/JobPostingMapperForUpdate.java
@@ -2,37 +2,25 @@ package com.example.hirehub.mapper;
 
 import com.example.hirehub.model.entity.jobPostingEntities.JobPostingEntity;
 import com.example.hirehub.model.entity.jobPostingEntities.JobPostingInfoEntity;
-import com.example.hirehub.model.request.jobPostingRequest.JobPostingUpdateRequest;
 import com.example.hirehub.model.response.jobPostingResponse.JobPostingUpdateResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE, makeFinal = true)
 public class JobPostingMapperForUpdate {
 
-    public JobPostingEntity toEntityForUpdate(JobPostingUpdateRequest request){
-        JobPostingEntity entity = new JobPostingEntity();
-        entity.setJobTitle(request.getJobTitle());
-        entity.setExpiredDate(request.getExpiredDate());
-
-        JobPostingInfoEntity infoEntity = new JobPostingInfoEntity();
-        infoEntity.setEduReq(request.getEduReq());
-        infoEntity.setExpLevel(request.getExpLevel());
-        infoEntity.setSkills(request.getSkills());
-        infoEntity.setLocation(request.getLocation());
-        infoEntity.setWorkType(request.getWorkType());
-        infoEntity.setWorkPlace(request.getWorkPlace());
-        infoEntity.setSalary(request.getSalary());
-        infoEntity.setPosition(request.getPosition());
-
-        entity.setJobPostingInfoEntity(infoEntity);
-        infoEntity.setJobPostingEntity(entity);
-
-        return entity;
-    }
-
-    public JobPostingUpdateResponse toResponseForUpdate(JobPostingEntity entity){
+    public JobPostingUpdateResponse toResponseForUpdate(JobPostingEntity entity) {
         JobPostingUpdateResponse response = new JobPostingUpdateResponse();
 
         response.setJobTitle(entity.getJobTitle());
         response.setExpiredDate(entity.getExpiredDate());
+
         response.setEduReq(entity.getJobPostingInfoEntity().getEduReq());
         response.setExpLevel(entity.getJobPostingInfoEntity().getExpLevel());
         response.setSkills(entity.getJobPostingInfoEntity().getSkills());
@@ -43,7 +31,12 @@ public class JobPostingMapperForUpdate {
         response.setPosition(entity.getJobPostingInfoEntity().getPosition());
 
         return response;
+
+
+
+
     }
+
 
 
 
