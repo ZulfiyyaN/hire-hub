f4afb4affbc8eb4d22a785dde9cc2f2a3abe567e
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-30 17:37:14 +0400

    create auth controller and APIs for login

---
+++ src/main/java/com/example/hirehub/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.hirehub.controller;
+
+import com.example.hirehub.model.request.AuthRequest;
+import com.example.hirehub.model.response.AuthResponse;
+import com.example.hirehub.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthController {
+
+    AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login_admin")
+    public ResponseEntity<AuthResponse> loginForAdmin(@Valid @RequestBody AuthRequest request) {
+        AuthResponse response = authService.loginForAdmin(request);
+        return ResponseEntity.ok(response);
+    }
+}
