7f81ba2308823120d210649713935bf3e0bb47fc
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-30 17:41:59 +0400

    added new delete method for candidate

--- src/main/java/com/example/hirehub/service/candidateService/CandidateService.java
+++ src/main/java/com/example/hirehub/service/candidateService/CandidateService.java
@@ -12,4 +12,5 @@ public interface CandidateService {
 
     public CandidateUpdateResponse candidateUpdate(String email, CandidateUpdateRequest request);
 
+    public boolean deleteProfilForCandidate(String email);
 }
