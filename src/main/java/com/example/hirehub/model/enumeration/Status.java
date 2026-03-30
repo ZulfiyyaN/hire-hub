b3a06133283b64aad1a5d360ae5c8b6c2b4783d9
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-30 17:46:49 +0400

    modified objects in Status enum

--- src/main/java/com/example/hirehub/model/enumeration/Status.java
+++ src/main/java/com/example/hirehub/model/enumeration/Status.java
@@ -1,7 +1,8 @@
 package com.example.hirehub.model.enumeration;
 
 public enum Status {
-    ACTIVE,
-    DEACTIVATED,
-    PENDING;
+    PENDING, //after register the profil is in PENDING status untill admin accept
+    ACTIVE, //after admin accept
+    DELETED, //when user (candidate or company) delete the profil or job post
+    BLOCKED; //when admin block the profil or job post
 }
