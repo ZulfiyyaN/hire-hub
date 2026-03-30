dff80b23c41441e99e9e0099d4f96d9581f9e20d
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-30 17:43:02 +0400

    implement new delete method

--- src/main/java/com/example/hirehub/service/candidateService/CandidateServiceImpl.java
+++ src/main/java/com/example/hirehub/service/candidateService/CandidateServiceImpl.java
@@ -4,13 +4,17 @@ import com.example.hirehub.exception.AlreadyExistsException;
 import com.example.hirehub.exception.CandidateNotFoundException;
 import com.example.hirehub.mapper.CandidateMapperForRegister;
 import com.example.hirehub.mapper.CandidateMapperForUpdate;
+import com.example.hirehub.model.entity.UserEntity;
 import com.example.hirehub.model.entity.candidateEntities.CandidateEntity;
 import com.example.hirehub.model.entity.candidateEntities.CandidateInfoEntity;
+import com.example.hirehub.model.enumeration.Role;
+import com.example.hirehub.model.enumeration.Status;
 import com.example.hirehub.model.request.candidateRequest.CandidateRegisterRequest;
 import com.example.hirehub.model.request.candidateRequest.CandidateUpdateRequest;
 import com.example.hirehub.model.response.candidateResponse.CandidateRegisterResponse;
 import com.example.hirehub.model.response.candidateResponse.CandidateUpdateResponse;
 import com.example.hirehub.repository.CandidateRepository;
+import com.example.hirehub.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -27,11 +31,12 @@ import java.util.Optional;
 @Transactional
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-
 public class CandidateServiceImpl implements CandidateService {
     CandidateRepository candidateRepository;
     CandidateMapperForRegister candidateMapper;
     CandidateMapperForUpdate candidateMapperForUpdate;
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
     @Override
     public CandidateRegisterResponse candidateRegister(CandidateRegisterRequest request) {
@@ -43,8 +48,24 @@ public class CandidateServiceImpl implements CandidateService {
             log.warn("Phone {} already exists", request.getPhone());
             throw new AlreadyExistsException("Candidate with this phone already exists");
         }
+
         CandidateEntity candidateEntity = candidateMapper.toCandidate(request);
-        candidateEntity = candidateRepository.save(candidateEntity);
+
+        UserEntity user = new UserEntity();
+
+        user.setName(request.getName());
+        user.setSurname(request.getSurname());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.CANDIDATE);
+        user.setStatus(Status.ACTIVE);
+
+        candidateEntity.setUser(user);
+        user.setCandidateEntity(candidateEntity);
+        userRepository.save(user);
+        candidateRepository.save(candidateEntity);
+
+
         CandidateRegisterResponse response = candidateMapper.toResponse(candidateEntity);
         log.info("{} is registered", request.getName() + " " + request.getSurname());
         return response;
@@ -94,4 +115,18 @@ public class CandidateServiceImpl implements CandidateService {
         CandidateUpdateResponse response = candidateMapperForUpdate.toResponseForUpdate(entity.get());
         return response;
     }
+
+    @Override
+    public boolean deleteProfilForCandidate(String email) {
+        Optional<UserEntity> optionalCandidate = userRepository.findByEmail(email);
+        if (optionalCandidate.isEmpty()) {
+            log.warn("Candidate not found with {} ", email);
+            throw new CandidateNotFoundException("Candidate not found: " + email);
+        }
+        optionalCandidate.get().setStatus(Status.DELETED);
+        userRepository.save(optionalCandidate.get());
+        return true;
+    }
+
+
 }
