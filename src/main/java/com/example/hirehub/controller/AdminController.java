5bbd7819ebd08a597a2471d5f0fb43dcf252fa0c
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-03-30 17:41:04 +0400

    create controller for admin APIs

---
+++ src/main/java/com/example/hirehub/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.example.hirehub.controller;
+
+import com.example.hirehub.model.entity.UserEntity;
+import com.example.hirehub.model.enumeration.Status;
+import com.example.hirehub.service.adminService.AdminService;
+import com.example.hirehub.service.AuthService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class AdminController {
+
+    AdminService adminService;
+
+
+    @PostMapping("/change_status")
+    public ResponseEntity<?> make_Active(@RequestParam String email, @RequestParam Status status){
+        adminService.changeStatus(email, status);
+        return ResponseEntity.accepted().build();
+    }
+
+
+
+
+
+}
