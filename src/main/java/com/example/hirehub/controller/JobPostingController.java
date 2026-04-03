c8c8c9ba2ab9c925299ac14238902ac364f14de5
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-03 23:10:24 +0400

    Update JobPostingController: add @Slf4j, authentication null check, and generalize response type

--- src/main/java/com/example/hirehub/controller/JobPostingController.java
+++ src/main/java/com/example/hirehub/controller/JobPostingController.java
@@ -10,10 +10,12 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/job_posting")
 @RequiredArgsConstructor
@@ -22,8 +24,12 @@ public class JobPostingController {
     JobPostingService jobPostingService;
 
     @PostMapping("/create")
-    public ResponseEntity<JobPostingCreateResponse> createJobPost(@RequestBody @Valid JobPostingCreateRequest request,
+    public ResponseEntity<?> createJobPost(@RequestBody @Valid JobPostingCreateRequest request,
                                                                   Authentication authentication){
+        if (authentication == null) {
+            log.warn("Access denied: No authentication found");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Candidate not found!");
+        }
         String email = authentication.getName();
        JobPostingCreateResponse response =  jobPostingService.createJobPost(email, request);
         System.out.println(email);
@@ -32,10 +38,15 @@ public class JobPostingController {
 
 
     @PutMapping("/update")
-    public ResponseEntity<JobPostingUpdateResponse> updateJobPost(@RequestBody @Valid JobPostingUpdateRequest request,
+    public ResponseEntity<?> updateJobPost(@RequestBody @Valid JobPostingUpdateRequest request,
+                                                                  @RequestParam Long id,
                                                                   Authentication authentication ){
+        if (authentication == null) {
+            log.warn("Access denied: No authentication found");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Candidate not found!");
+        }
         String email = authentication.getName();
-        JobPostingUpdateResponse response = jobPostingService.updateJobPost(email, request);
+        JobPostingUpdateResponse response = jobPostingService.updateJobPost(email, id, request);
         return  ResponseEntity.ok(response);
     }
 
