e06e9d3cfc554b1db69934d50338567ba83a6413
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-03 23:14:09 +0400

    created findByIdNative method inJob Posting Repository with native quesry

--- src/main/java/com/example/hirehub/repository/JobPostingRepository.java
+++ src/main/java/com/example/hirehub/repository/JobPostingRepository.java
@@ -1,8 +1,17 @@
 package com.example.hirehub.repository;
 
+import com.example.hirehub.model.entity.UserEntity;
 import com.example.hirehub.model.entity.jobPostingEntities.JobPostingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
+
+    @Query(value = "SELECT * FROM job_postings jp WHERE jp.id = :id AND jp.status IN('PENDING', 'ACTIVE') ", nativeQuery = true)
+    Optional<JobPostingEntity> findByIdNative(@Param("id") Long id);
+
 
 }
