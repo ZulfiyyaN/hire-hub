dbd98ddecdfdabab6cd32486028c1a4b1f6507fc
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-03 13:38:34 +0400

    added findBy id method with native query in UserRepository

--- src/main/java/com/example/hirehub/repository/UserRepository.java
+++ src/main/java/com/example/hirehub/repository/UserRepository.java
@@ -2,12 +2,22 @@ package com.example.hirehub.repository;
 
 import com.example.hirehub.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
    Optional<UserEntity> findByEmail(String email);
 
+
+   @Query(value = "SELECT * FROM user_entity u WHERE u.id = :id", nativeQuery = true)
+   Optional<UserEntity> findByIdNative(@Param("id") Long id);
+
+
 //    Optional<UserEntity> findById(Long id);
+
+
 
 }
