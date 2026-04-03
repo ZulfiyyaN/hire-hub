aca65395012a28beec699a84a54274daf16ef355
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-03 13:34:01 +0400

    change parametr at change status method for admin

--- src/main/java/com/example/hirehub/service/adminService/AdminService.java
+++ src/main/java/com/example/hirehub/service/adminService/AdminService.java
@@ -5,7 +5,7 @@ import com.example.hirehub.model.enumeration.Status;
 
 public interface AdminService {
 
-    boolean changeStatus(String email, Status status);
+    boolean changeStatus(Long id, Status status);
 //    void makeBlockedCandidate(Long id, String reason);
 //    List<CandidateEntity> getAllPendingCandidates(); // Yalnız gözləmədə olanlar
 //    void blockUser(String email);
