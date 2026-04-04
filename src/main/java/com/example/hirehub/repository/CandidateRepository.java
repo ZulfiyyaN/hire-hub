022e9d41e32ff944737f1296b59c6c738a652e7c
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-04 21:51:36 +0400

    adedd findAll method to CandidateRepository

--- src/main/java/com/example/hirehub/repository/CandidateRepository.java
+++ src/main/java/com/example/hirehub/repository/CandidateRepository.java
@@ -1,14 +1,20 @@
 package com.example.hirehub.repository;
+
+import com.example.hirehub.model.entity.UserEntity;
 import com.example.hirehub.model.entity.candidateEntities.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
 
     boolean existsByEmail(String email);
+
     boolean existsByPhone(String phone);
 
-     Optional<CandidateEntity> findByEmail(String email);
+    Optional<CandidateEntity> findByEmail(String email);
+
+    List<CandidateEntity> findAll();
 }
 
