b2652be98cfb106da8ff5a977d3cda6b864db09d
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-04 12:22:55 +0400

    create change status method for job posting

--- src/main/java/com/example/hirehub/service/adminService/AdminService.java
+++ src/main/java/com/example/hirehub/service/adminService/AdminService.java
@@ -2,11 +2,14 @@ package com.example.hirehub.service.adminService;
 
 
 import com.example.hirehub.model.enumeration.Status;
+import com.example.hirehub.model.enumeration.StatusJobPost;
 
 public interface AdminService {
 
     boolean changeStatus(Long id, Status status);
-//    void makeBlockedCandidate(Long id, String reason);
+
+    boolean changeStatusJobPost(Long id, StatusJobPost status);
+
 //    List<CandidateEntity> getAllPendingCandidates(); // Yalnız gözləmədə olanlar
 //    void blockUser(String email);
 }
