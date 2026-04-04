c29c36a15fc5ea31176da2ed2c34ad4066388ca0
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-04 16:43:31 +0400

    add native query to find users by status in UserRepository

--- src/main/java/com/example/hirehub/repository/UserRepository.java
+++ src/main/java/com/example/hirehub/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.hirehub.repository;
 
 import com.example.hirehub.model.entity.UserEntity;
+import com.example.hirehub.model.enumeration.Role;
+import com.example.hirehub.model.enumeration.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
@@ -15,8 +18,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
    @Query(value = "SELECT * FROM user_entity u WHERE u.id = :id", nativeQuery = true)
    Optional<UserEntity> findByIdNative(@Param("id") Long id);
 
+   @Query(value = "SELECT * FROM user_entity WHERE status = :status", nativeQuery = true)
+   List<UserEntity> findAllByStatus(@Param("status")String status);
 
-//    Optional<UserEntity> findById(Long id);
 
 
 
