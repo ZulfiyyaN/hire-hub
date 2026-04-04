d023669ae60fcd6702bd550f884ed8b5a53139ba
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-04 21:53:14 +0400

    created Candidate Response record

--- src/main/java/com/example/hirehub/model/response/candidateResponse/CandidateResponse.java
+++ src/main/java/com/example/hirehub/model/response/candidateResponse/CandidateResponse.java
@@ -1,4 +1,18 @@
 package com.example.hirehub.model.response.candidateResponse;
 
-public record CandidateResponse() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public record CandidateResponse(Long id,
+                                String name,
+                                String surname,
+                                String email,
+                                String phone,
+                                String gender,
+                                @JsonFormat(pattern = "yyyy-MM-dd")
+                                LocalDate dateOfBirth,
+                                String education,
+                                String workExperience,
+                                String knowledge) {
 }
