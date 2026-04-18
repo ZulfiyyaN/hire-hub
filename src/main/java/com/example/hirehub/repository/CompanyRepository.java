f15ce0d23dfcf576a648094933b2d84a4bd2fb59
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-18 17:25:46 +0400

    added new method to fing byemail and user

--- src/main/java/com/example/hirehub/repository/CompanyRepository.java
+++ src/main/java/com/example/hirehub/repository/CompanyRepository.java
@@ -3,6 +3,8 @@ package com.example.hirehub.repository;
 import com.example.hirehub.model.entity.candidateEntities.CandidateEntity;
 import com.example.hirehub.model.entity.companyEntities.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,12 @@ public interface CompanyRepository extends JpaRepository <CompanyEntity, Long>{
 
     boolean existsByEmail(String email);
     Optional<CompanyEntity> findByEmail(String email);
+
+    @Query("SELECT c FROM CompanyEntity c JOIN FETCH c.user u WHERE c.email = :email")
+    Optional<CompanyEntity> findByEmailWithUser(@Param("email") String email);
+
+
+
+
+
 }
