7c2fe6d4b347f0b7ce81ae71829481232371e683
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-20 23:43:10 +0400

    modified method exception handler

--- src/main/java/com/example/hirehub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hirehub/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.hirehub.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.boot.web.error.Error;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -31,7 +33,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleNotValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleNotValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
         Map<String, String> details = new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(error ->
@@ -41,10 +43,40 @@ public class GlobalExceptionHandler {
         ErrorResponse response = new ErrorResponse();
 
         response.setPath(request.getRequestURI());
-        response.setMessage("Validation failed!");
+        response.setMessage(ex.getMessage());
         response.setTimestamp(LocalDateTime.now());
         response.setDetails(details);
-        return response;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
+
+
+    @ExceptionHandler(JobPostingNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleJobPostingNotFound(JobPostingNotFoundException ex, HttpServletRequest request){
+        log.warn("Not Found: " + ex.getMessage());
+        ErrorResponse response = new ErrorResponse();
+
+        response.setPath(request.getRequestURI());
+        response.setMessage(ex.getMessage());
+        response.setTimestamp(LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleallException (Exception ex, HttpServletRequest request){
+        log.error("Unhandled exception", ex);
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("something went wrong");
+        response.setTimestamp(LocalDateTime.now());
+
+        return  ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
 
 }
