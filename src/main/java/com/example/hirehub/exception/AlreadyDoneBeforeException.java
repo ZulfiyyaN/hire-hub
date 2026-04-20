af7000bb31be45bd80284dbdee988e3c2af613af
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-20 23:57:26 +0400

    create new exception - AlreadyDoneBeforeException

---
+++ src/main/java/com/example/hirehub/exception/AlreadyDoneBeforeException.java
@@ -0,0 +1,7 @@
+package com.example.hirehub.exception;
+
+public class AlreadyDoneBeforeException extends RuntimeException{
+    public AlreadyDoneBeforeException(String message) {
+        super(message);
+    }
+}
