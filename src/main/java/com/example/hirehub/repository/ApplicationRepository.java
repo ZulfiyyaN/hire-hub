911dba28f9ada6b28f277be2145ef3fc75bf3baf
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-22 00:32:15 +0400

    created findall method in Application Repository

--- src/main/java/com/example/hirehub/repository/ApplicationRepository.java
+++ src/main/java/com/example/hirehub/repository/ApplicationRepository.java
@@ -2,8 +2,13 @@ package com.example.hirehub.repository;
 
 import com.example.hirehub.model.entity.ApplicationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ApplicationRepository extends JpaRepository<ApplicationEntity, Long> {
 
     boolean existsByJobPosting_IdAndCandidate_Id(Long jobPostingId, Long candidateId);
+
+    List<ApplicationEntity> findAll();
 }
