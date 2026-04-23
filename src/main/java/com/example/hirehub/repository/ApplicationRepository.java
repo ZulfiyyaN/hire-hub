424c6670477dfbff2e413d7d74888288cf0657bb
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-23 22:37:26 +0400

    create findById method in application Repository

--- src/main/java/com/example/hirehub/repository/ApplicationRepository.java
+++ src/main/java/com/example/hirehub/repository/ApplicationRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<ApplicationEntity, Long> {
 
     boolean existsByJobPosting_IdAndCandidate_Id(Long jobPostingId, Long candidateId);
 
     List<ApplicationEntity> findAll();
+
+    public Optional<ApplicationEntity> findById(Long id);
 }
