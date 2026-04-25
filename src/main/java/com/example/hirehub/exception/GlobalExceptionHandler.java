a71507ac597b6bc18418404d45166aa7f6c8ba8f
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-04-25 23:38:18 +0400

    added new handles for exceptions in GlobalExceptionHandler

--- src/main/java/com/example/hirehub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hirehub/exception/GlobalExceptionHandler.java
@@ -21,7 +21,7 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(AlreadyExistsException.class)
-    public ErrorResponse handleAlreadyExists(AlreadyExistsException ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleAlreadyExists(AlreadyExistsException ex, HttpServletRequest request) {
         log.warn("Already exists: " + ex.getMessage());
         ErrorResponse response = new ErrorResponse();
 
@@ -29,19 +29,20 @@ public class GlobalExceptionHandler {
         response.setMessage("Already exists!");
         response.setTimestamp(LocalDateTime.now());
 
-        return response;
+        Map<String, String> details = new HashMap<>();
+        details.put("errorType", "ALREADY_EXISTS");
+        details.put("info", ex.getMessage());
+        response.setDetails(details);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleNotValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
         Map<String, String> details = new HashMap<>();
-
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 details.put(error.getField(), error.getDefaultMessage())
         );
-
         ErrorResponse response = new ErrorResponse();
-
         response.setPath(request.getRequestURI());
         response.setMessage(ex.getMessage());
         response.setTimestamp(LocalDateTime.now());
@@ -50,8 +51,127 @@ public class GlobalExceptionHandler {
     }
 
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllException(Exception ex, HttpServletRequest request) {
+        log.error("Unhandled exception", ex);
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("something went wrong");
+        response.setTimestamp(LocalDateTime.now());
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
+
+    @ExceptionHandler(AlreadyDoneBeforeException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyExists(AlreadyDoneBeforeException ex, HttpServletRequest request) {
+        log.warn("Already done: " + ex.getMessage());
+        ErrorResponse response = new ErrorResponse();
+
+        response.setPath(request.getRequestURI());
+        response.setMessage("Already exists!");
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info: ", "Already done");
+        response.setDetails(d);
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+
+    @ExceptionHandler(ApplicationNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleApplicationNotFound(ApplicationNotFoundException ex,
+                                                              HttpServletRequest request){
+
+        log.warn("Application not found!");
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("application not found");
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info: ", "Application not found");
+        response.setDetails(d);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+
+    @ExceptionHandler(CandidateNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCandidateNotFound(CandidateNotFoundException ex,
+                                                              HttpServletRequest request){
+
+        log.warn("Candidate not found!");
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("Candidate not found");
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info: ", "Candidate not found");
+        response.setDetails(d);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+
+    @ExceptionHandler(CompanyNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCompanyNotFound(ApplicationNotFoundException ex,
+                                                              HttpServletRequest request){
+
+        log.warn("Company not found!");
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("Company not found");
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info: ", "Company not found");
+        response.setDetails(d);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(IncorrectAgeException.class)
+    public ResponseEntity<ErrorResponse> handleIncorrectAge(IncorrectAgeException ex,
+                                                              HttpServletRequest request){
+
+        log.warn("Incorrect age!");
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("Incorrect age");
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info: ", "Age should be min 16");
+        response.setDetails(d);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+
+    @ExceptionHandler(IncorrectPasswordException.class)
+    public ResponseEntity<ErrorResponse> handleIncorrectPassword(IncorrectPasswordException ex,
+                                                      HttpServletRequest request){
+
+        log.warn("Incorrect password!");
+        ErrorResponse response = new ErrorResponse();
+        response.setPath(request.getRequestURI());
+        response.setMessage("Incorrect password");
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info: ", "Password is not correct!");
+        response.setDetails(d);
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
     @ExceptionHandler(JobPostingNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleJobPostingNotFound(JobPostingNotFoundException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleJobPostingNotFound(JobPostingNotFoundException ex, HttpServletRequest request) {
         log.warn("Not Found: " + ex.getMessage());
         ErrorResponse response = new ErrorResponse();
 
@@ -59,24 +179,47 @@ public class GlobalExceptionHandler {
         response.setMessage(ex.getMessage());
         response.setTimestamp(LocalDateTime.now());
 
+        Map<String, String> d = new HashMap<>();
+        d.put("info", "Job Post not found");
+        response.setDetails(d);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+
+    @ExceptionHandler(MismatchCompanyApplicationException.class)
+    public ResponseEntity<ErrorResponse> handleMismatchCompanyApplication(MismatchCompanyApplicationException ex, HttpServletRequest request) {
+        log.warn("Mismatch Company: " + ex.getMessage());
+        ErrorResponse response = new ErrorResponse();
+
+        response.setPath(request.getRequestURI());
+        response.setMessage(ex.getMessage());
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info", "Mismatch Company");
+        response.setDetails(d);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex, HttpServletRequest request) {
+        log.warn("User not found: " + ex.getMessage());
+        ErrorResponse response = new ErrorResponse();
+
+        response.setPath(request.getRequestURI());
+        response.setMessage(ex.getMessage());
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> d = new HashMap<>();
+        d.put("info", "User not found");
+        response.setDetails(d);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
 
 
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleallException (Exception ex, HttpServletRequest request){
-        log.error("Unhandled exception", ex);
-        ErrorResponse response = new ErrorResponse();
-        response.setPath(request.getRequestURI());
-        response.setMessage("something went wrong");
-        response.setTimestamp(LocalDateTime.now());
-
-        return  ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(response);
-    }
 
 
 }
